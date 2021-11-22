9e16395acba442ff2928561f882a40b12d043823
nicholas sistovaris <nikolaos.sistovaris@gmail.com>
2021-11-22 20:33:13 +0000

    few changes

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,11 @@
-require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-waffle');
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: '0.8.0',
+  networks: {
+    rinkeby: {
+      url: 'https://eth-rinkeby.alchemyapi.io/v2/KZLvChn-KFv6fPhRbC4g6V_QemNTBwwF',
+      accounts: ['a97e798496c5ce7f90e35e516b3836bc8046027e8e6252ab4b4a310ac7513d50'],
+    },
+  },
 };
